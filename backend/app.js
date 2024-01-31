ac8c68a1767205f436ebbf657290346d529fbafb
Muhammad Haris <iharisimran@gmail.com>
2024-01-31 19:14:57 +0500

    enhanced security

--- backend/app.js
+++ backend/app.js
@@ -1,26 +1,40 @@
-const crypto = require('crypto'),
-    key = crypto.randomBytes(32),
-    iv = crypto.randomBytes(16);
+const crypto = require('crypto');
 
 const encrypt = data => {
-    data = JSON.stringify({ d: data })
-    const cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(key), iv);
-    let encrypted = cipher.update(data);
-    encrypted = Buffer.concat([encrypted, cipher.final()]);
-    return { iv: iv.toString('hex'), encryptedData: encrypted.toString('hex') };
+    try {
+        const key = crypto.randomBytes(32),
+            iv = crypto.randomBytes(16);
+        data = JSON.stringify({ d: data });
+        const cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(key), iv);
+        let encrypted = cipher.update(data, 'utf-8', 'hex');
+        encrypted += cipher.final('hex');
+        return `${iv.toString('hex')},${encrypted},${key.toString('hex')}`;
+    } catch (err) {
+        console.log(err.message);
+        return null;
+    };
 };
 
 const decrypt = encryptionKey => {
-    const iv = Buffer.from(encryptionKey.iv, 'hex'),
-        encrypted = Buffer.from(encryptionKey.encryptedData, 'hex'),
-        decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(key), iv);
-    let decrypted = decipher.update(encrypted);
-    decrypted = Buffer.concat([decrypted, decipher.final()]);
-    return JSON.parse(decrypted.toString());
+    try {
+        encryptionKey =encryptionKey.split(",")
+        const iv = Buffer.from(encryptionKey[0], 'hex'),
+            encrypted = Buffer.from(encryptionKey[1], 'hex'),
+            key = Buffer.from(encryptionKey[2], 'hex'),
+            decipher = crypto.createDecipheriv('aes-256-cbc', key, iv);
+        let decrypted = decipher.update(encrypted, 'hex', 'utf-8');
+        decrypted += decipher.final('utf-8');
+        if (decrypted) {
+            decrypted = JSON.parse(decrypted).d;
+            return decrypted;
+        } else { return null };
+    } catch (err) {
+        console.log(err.message);
+        return null;
+    };
 };
 
-
-const encryptedData = encrypt("This is the secret data that only I and the Government of United States know! 🤫");
+const encryptedData = encrypt("This is the secret data that only I and the Government of United States knows! 🤫");
 console.log(encryptedData);
 
 const decryptedData = decrypt(encryptedData);
--- frontend/app.js
+++ frontend/app.js
@@ -1,12 +1,13 @@
 import CryptoJS from "crypto-js";
 
-export default function App() {
-  const data = "This is the secret data that only I and the Government of United States know! 🤫",
+export default function encryption() {
+  const data = "This is the secret data that only I and the Government of United States knows! 🤫",
     secretKey = "Qwerty123@",
     encrypted = CryptoJS.AES.encrypt(data, secretKey),
     decrypted = CryptoJS.AES.decrypt(encrypted, secretKey),
     formatted = decrypted.toString(CryptoJS.enc.Utf8);
 
   console.log(`Origional: ${data}`);
+  console.log(`Encrypted: ${encrypted}`);
   console.log(`Decrypted: ${formatted}`);
-}
+};
